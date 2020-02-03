f0099066e9650daf42980dde166338004ba2524a
Tommy Palmer <hi@tommyp.org>
2020-02-03 13:36:22 +0000

    fix redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Mashup from "./components/mashup";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Mashup />
+      <Router>
+        <Switch>
+          <Route path="/up">
+            <Mashup />
+          </Route>
+          <Redirect from="/" to="/up"/>
+        </Switch>
+      </Router>
     </div>
   );
 }
